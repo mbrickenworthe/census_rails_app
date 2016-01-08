69444e5c5b195302b7e31767e4ece90fbf70a174
mbrickenworthe <adamwinters456@gmail.com>
2016-01-07 23:30:53 -0500

    Got the famous vs family person working as well as tweaking some GoogleMaps items

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,15 +1,25 @@
 class PeopleController < ApplicationController
 
-before_action :authenticate_user!
+before_action :authenticate_user!, except: [:famous_new, :create]
 
   def new
     if created_first_admin
       @person = Person.new(name: current_user.name)
       @text = "Create Your Profile Info"
+      @name = "Your Name"
     else
       @person = Person.new(user: current_user)
       @text = "Create A Family Member"
+      @name = "Your Family Member's Name"
     end
+    @famous = false
+  end
+
+  def famous_new
+    @person = Person.new
+    @text = "Create A Famous Person!"
+    @name = "Famous Person's Name"
+    @famous = true
   end
 
   def create
@@ -20,6 +30,9 @@ before_action :authenticate_user!
       sex = true
     end
     if @person.update_attributes(person_params) && @person.update_attributes(male: sex, active_user: created_first_admin, user: current_user)
+      if params[:person][:famous] == "true"
+        @person.update_attributes(user: nil)
+      end
       cencus = CencusCall.new(@person)
       PersonDatum.create(json_hash: cencus.call_data, person: @person)
       redirect_to root_path
--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -2,7 +2,9 @@ class SplashController < ApplicationController
 
   def page
     @famous_people = Person.where(user: nil)
-    @family_people = Person.where(user: current_user)
+    if signed_in?
+      @family_people = Person.where(user: current_user)
+    end
   end
 
 
