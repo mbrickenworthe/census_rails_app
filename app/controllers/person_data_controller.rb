8c1110b6beee4f0812393258dd05e86c4c76f811
mbrickenworthe <adamwinters456@gmail.com>
2016-01-12 18:58:01 -0500

    Google controller update.

--- app/controllers/person_data_controller.rb
+++ app/controllers/person_data_controller.rb
@@ -26,6 +26,10 @@ class PersonDataController < ApplicationController
     @key = MyApp.config.google_api_key
     @current_user_people = Person.where(:user => [current_user, nil])
     @google_hash = {}
+    @circle_one = {}
+    @circle_two = {}
+    @circle_three = {}
+    @circle_four = {}
     @current_user_person_object = Person.where(user: current_user, active_user: true).first
     @person_data_map_data.each do |personal_data|
       @google_hash[(personal_data.person.name)] = GoogleMapsInfoHelper.new(personal_data).final_hash
