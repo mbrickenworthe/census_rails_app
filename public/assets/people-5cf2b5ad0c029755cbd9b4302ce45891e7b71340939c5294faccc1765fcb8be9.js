742d15718e125056f02b2482740bd0894dd0fc3b
mbrickenworthe <adamwinters456@gmail.com>
2016-01-16 11:33:12 -0500

    Precompiling my assets for heroku

---
+++ public/assets/people-5cf2b5ad0c029755cbd9b4302ce45891e7b71340939c5294faccc1765fcb8be9.js
@@ -0,0 +1,9 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+ document.getElementById("test").addEventListener("click", function(event) {
+    // display the current click count inside the clicked div
+    console.log("Hey");
+  }, false);
+
